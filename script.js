7e7219b722793a727fa3ddb49df8a870ff0afe4e
denzel-04 <jdg32113@gmail.com>
2025-02-03 23:17:06 -0500

    3 Files

---
+++ script.js
@@ -0,0 +1,39 @@
+body {
+    text-align: center;
+    font-family: 'Arial', sans-serif;
+    background-color: #ffccd5;
+    color: #d63384;
+}
+
+.container {
+    margin-top: 100px;
+}
+
+h1 {
+    font-size: 30px;
+    font-weight: bold;
+}
+
+.buttons {
+    margin-top: 20px;
+}
+
+button {
+    font-size: 18px;
+    padding: 10px 20px;
+    margin: 10px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: 0.3s;
+}
+
+#yesButton {
+    background-color: #ff4081;
+    color: white;
+}
+
+#noButton {
+    background-color: #cccccc;
+    color: black;
+}
